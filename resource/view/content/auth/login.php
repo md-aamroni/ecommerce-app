d071a76aa2e2abac2e997048ad7fb779a6ba70f4
unknown <azharulip@gmail.com>
2021-07-05 15:47:52 +0600

    adminLogin

--- resource/view/content/auth/login.php
+++ resource/view/content/auth/login.php
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-
 <?php
+
 use config\DB;
 
-$q="select * from admins";
+$q = "select * from admins";
 /*
 $result=mysqli_query($con,$q);
 if (mysqli_num_rows($result) > 0) {
@@ -16,34 +15,31 @@ echo"<pre>";
 echo $con;
 echo"</pre>";
 */
-$admin_name=$_GET['full_name'];
+$admin_name = $_GET['full_name'];
 echo $admin_name;
 ?>
 <div class="contaner-fluid">
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <!-- Tabs Titles -->
 
-    <!-- Icon -->
-    <div class="fadeIn first">
-     <h1>Admin Login</h1>
+      <!-- Icon -->
+      <div class="fadeIn first">
+        <h1>Admin Login</h1>
+      </div>
+
+      <!-- Login Form -->
+      <form>
+        <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+        <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+        <input type="submit" class="fadeIn fourth" value="Log In">
+      </form>
+
+      <!-- Remind Passowrd -->
+      <div id="formFooter">
+        <a class="underlineHover" href="#">Forgot Password?</a>
+      </div>
+
     </div>
-
-    <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
-    </form>
-
-    <!-- Remind Passowrd -->
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
   </div>
 </div>
-</div>
-=======
-admin log in page 
->>>>>>> 45dd8b8710edf0ea14cb98377af651d9a277b4b7
