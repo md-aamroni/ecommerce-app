6c0538fec15ea9eadafbf4d945248fb5d193584b
unknown <azharulip@gmail.com>
2021-07-16 10:01:45 +0600

    merge git

--- app/Models/SliderModel.php
+++ app/Models/SliderModel.php
@@ -1,11 +1,5 @@
 <?php
-<<<<<<< HEAD
 namespace App\Models;
-=======
-
-namespace App\Models;
-
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
 use Exception;
 use App\Eloquent\EloquentORM;
 
@@ -14,7 +8,6 @@ class SliderModel extends EloquentORM
 	protected $table = 'sliders';
 	protected $id;
 	protected $title;
-<<<<<<< HEAD
 	protected $sub_title;
 	protected $banner;
 	protected $status;
@@ -32,46 +25,15 @@ class SliderModel extends EloquentORM
 						)
 						VALUES (
 							:S_CAT_ID, :TITLE,:S_TITLE,  :S_BANNER, :S_STATUS, :CREATED_AT
-=======
-   protected $sub_title;
-	protected $images;
-	protected $alt_text;
-	protected $sequence;
-	protected $is_activ;
-
-	protected function addNewSlider( $title,$sub_title, $images, $alt_text,$is_activ)
-	{
-		$this->title	= $this->encode($title);
-      $this->sub_title	= $this->encode($sub_title);
-		$this->images	= $this->encode($images);
-      $this->alt_text	= $this->encode($alt_text);
-		$this->sequence	= 'Not Yet';
-		$this->is_activ	= !empty($this->encode($is_activ)) ? $this->encode($is_activ) : 'Active';
-
-		$sqlCode	= "INSERT INTO $this->table (
-							 `title`, `sub_title`, `images`, `alt_text`, `sequence`, `is_active`, `created_at`
-						)
-						VALUES (
-							 :S_TITLE, :S_SUB_TITLE, :S_IMAGES, :S_ALT_TEXT, :S_SEQUENCE, :S_IS_ACTIVE, :CREATED_AT
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
 						)";
 
 		$queries	= $this->connection->prepare($sqlCode);
 		$bindParam = array(
-<<<<<<< HEAD
 			':S_CAT_ID'		=> $this->id,
 			':TITLE'		=> $this->title,
 			':S_TITLE'		=> $this->sub_title,
 			':S_BANNER'		=> $this->banner,
 			':S_STATUS'		=> $this->status,
-=======
-			':S_TITLE'		=> $this->title,
-			':S_SUB_TITLE'	=> $this->sub_title,
-			':S_IMAGES'		=> $this->images,
-			':S_ALT_TEXT'		=> $this->alt_text,
-			':S_SEQUENCE'		=> $this->sequence,
-         ':S_IS_ACTIVE'		=> $this->is_activ,
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
 			':CREATED_AT'	=> date("Y-m-d H:i:s")
 		);
 
@@ -101,7 +63,6 @@ class SliderModel extends EloquentORM
 	}
 
 
-<<<<<<< HEAD
 	protected function updateSlider($title,$sub_title, $banner, $status, $id)
 	{
 		
@@ -114,40 +75,16 @@ class SliderModel extends EloquentORM
 		$sqlCode = "UPDATE $this->table
                   SET
 							`id` = :S_ID, `title` = :S_TITLE, `sub_title` = :S_sub_title, `banner` = :S_BANNER, `status` = :S_STATUS, `updated_at` = :UPDATED_AT
-=======
-	protected function updateSlider( $title, $sub_title, $images, $alt_text, $is_active, $id)
-	{
-		$this->title	   = $this->encode($title);
-      $this->sub_title	= $this->encode($sub_title);
-      $this->images	   = $this->encode($images);
-		$this->alt_text	= $this->encode($alt_text);
-		$this->is_active	= $this->encode($is_active);
-		$this->id	      = $this->encode($id);
-
-		
-
-		$sqlCode = "UPDATE $this->table
-                  SET
-							`title` = :S_TITLE, `sub_title` = :S_SUBTITLE, `images` = :S_IMAGES, `alt_text` = :S_ALT, `is_active` = :S_IS_ACTIVE, `updated_at` = :UPDATED_AT
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
                   WHERE
 							`id` = :UPDATE_ID";
 
 		$queries = $this->connection->prepare($sqlCode);
 		$values  = array(
-<<<<<<< HEAD
 			':S_ID'		=> $this->id,
 			':S_TITLE'		=> $this->title,
 			':S_sub_title'		=> $this->sub_title,
 			':S_BANNER'		=> $this->banner,
 			':S_STATUS'		=> $this->status,
-=======
-			':S_TITLE'		=> $this->title,
-			':S_SUBTITLE'		=> $this->sub_title,
-			':S_IMAGES'		=> $this->images,
-			':S_ALT'		   => $this->alt_text,
-			':S_IS_ACTIVE'	=> $this->is_active,
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
 			':UPDATED_AT'	=> date("Y-m-d H:i:s"),
 			':UPDATE_ID'	=> $this->id
 		);
@@ -171,7 +108,3 @@ class SliderModel extends EloquentORM
 		}
 	}
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
--- config/storage.php
+++ config/storage.php
@@ -5,9 +5,5 @@ $GLOBALS['upDir'] = [
 	'admin'	=> 'storage/admins/',
 	'user'	=> 'storage/users/',
 	'subCat'	=> 'storage/sub-category/',
-<<<<<<< HEAD
 	'sliders'	=> 'storage/sliders/'
-=======
-	'slider'	=> 'storage/slider/'
->>>>>>> 99c19e599fdf3e0132a8bc71169a67b6a30ccb31
 ];
